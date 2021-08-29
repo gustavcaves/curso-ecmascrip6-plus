42d094fb68284bdd453572fc6e6a8430f045e59f
Gustavo Adolfo Cuevas García <gustavcaves@MacBook-Air-de-Gustav-Caves.local>
2021-08-29 17:27:53 +0200

    clases

---
+++ src/es6 junio de 2015/Clases, Módulos y Generadores.js
@@ -0,0 +1,40 @@
+// Clases
+
+class calculator {
+    constructor() {
+        this.valueA = 0;
+        this.valueB = 0;
+    }
+    sum(valueA, valueB) {
+        this.valueA = valueA;
+        this.valueB = valueB;
+        return this.valueA + this.valueB;
+    }
+}
+
+const calc = new calculator();
+console.log(calc.sum(2,2));
+
+// Import Modules
+
+import { hello } from './module';
+
+hello();
+console.log(hello())
+
+// Generators
+// Retorna una serie de valores
+
+function* helloWorld() {
+    if (true) {
+        yield "Hello, ";
+    }
+    if (true) {
+        yield "World";
+    }
+};
+
+const generatorHello = helloWorld();
+console.log(generatorHello.next().value);
+console.log(generatorHello.next().value);
+console.log(generatorHello.next().value);
